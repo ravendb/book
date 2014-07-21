1489862b5360695b762bd240f07041045d8434d2
Ayende Rahien <ayende@ayende.com>
2014-07-21 08:54:10 +0300

    Moving files around

--- build.ps1
+++ build.ps1
@@ -1,6 +1,11 @@
 Remove-Item  Output -Force -Recurse -ErrorAction SilentlyContinue | out-null 
 MkDir Output  | out-null
 
+$output = "Output\Inside RavenDB.pdf"
+
 pandoc --table-of-contents --toc-depth=3 --epub-metadata=metadata.xml `
 	--standalone --highlight-style=espresso --self-contained --chapters `
-	--number-sections -o "Output\RavenDB In Depth.pdf" .\title.txt .\Ch1.md .\Ch2.md
+	--number-sections -o $output .\title.txt `
+	.\Ch01\Ch01.md .\Ch02\Ch02.md
+
+start $output
