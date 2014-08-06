4f739600f92600ad69b61e53f9b0ab465737d8c4
Ayende Rahien <ayende@ayende.com>
2014-08-06 23:19:48 +0300

    Fixing build script

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ $output = "Output\Inside RavenDB.pdf"
 
 pandoc --table-of-contents --toc-depth=2 --epub-metadata=metadata.xml `
 	--standalone --highlight-style=espresso --self-contained --chapters `
-	--listings --latex-engine=xelatex `	--number-sections `
-	-o $output .\title.txt .\Intro.md .\Ch01\Ch01.md .\Ch02\Ch02.md .\Ch03\Ch03.md
+	--listings --latex-engine=xelatex --number-sections `
+	-o $output .\title.txt .\Intro.md .\Ch01\Ch01.md .\Ch02\Ch02.md .\Ch03\Ch03.md .\Ch04\Ch04.md
 
 start $output
