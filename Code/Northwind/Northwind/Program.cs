70d669a753c2756184db9b9ec848dbb860d0259b
Ayende Rahien <ayende@ayende.com>
2014-10-13 13:43:59 +0300

    More indexing discussion

--- Code/Northwind/Northwind/Program.cs
+++ Code/Northwind/Northwind/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Orders;
+using Raven.Abstractions.Replication;
 using Raven.Client;
 using Raven.Client.Document;
 using Raven.Client.Indexes;
@@ -19,11 +20,7 @@ namespace Northwind
 			_documentStore = new DocumentStore
 			{
 				Url = "http://localhost:8080",
-				DefaultDatabase = "Users_Master",
-				Conventions =
-				{
-					FailoverBehavior = FailoverBehavior.AllowReadsFromSecondariesAndWritesToSecondaries
-				}
+				DefaultDatabase = "Northwind",
 			};
 
 			_documentStore.Initialize();
@@ -33,16 +30,74 @@ namespace Northwind
 
 				using (var session = _documentStore.OpenSession())
 				{
-					var user = session.Load<User>(1);
-					user.Age++;
-					Console.WriteLine(user.Name);
-					session.SaveChanges();
+					session.Query<Orders_Totals.Result, Orders_Totals>()
+						.Where(x => x.Total > 10000)
+						.OfType<Order>()
+						.ToList();
 				}
 				Console.ReadLine();
 			}
 		}
 	}
 
+	public class Employees_Search : AbstractIndexCreationTask<Employee, Employees_Search.Result>
+	{
+		public class Result
+		{
+			public string Query;
+		}
+
+		public Employees_Search()
+		{
+			Map = employees =>
+				from employee in employees
+				select new
+				{
+					Query = new object[]
+					{
+						employee.FirstName,
+						employee.LastName,
+						employee.Territories
+					}
+				};
+		}
+	}
+
+	public class Orders_Products : AbstractIndexCreationTask<Order, Orders_Products.Result>
+	{
+		public class Result
+		{
+			public string Product;
+		}
+
+		public Orders_Products()
+		{
+			Map = orders =>
+				from order in orders
+				select new
+				{
+					Product = order.Lines.Select(x => x.Product)
+				};
+		}
+	}
+
+	public class Orders_Totals : AbstractIndexCreationTask<Order>
+	{
+		public class Result
+		{
+			public double Total;
+		}
+
+		public Orders_Totals()
+		{
+			Map = orders =>
+				from order in orders
+				select new
+				{
+					Total = order.Lines.Sum(l => (l.Quantity * l.PricePerUnit) * (1 - l.Discount))
+				};
+		}
+	}
 
 	public class User
 	{
