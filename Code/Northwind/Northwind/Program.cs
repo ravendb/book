e120bde3132de995ca229555cc0f9b4b1b0cf313
Ayende Rahien <ayende@ayende.com>
2015-02-05 08:31:04 +0200

    Starting to talk about practical full text search

--- Code/Northwind/Northwind/Program.cs
+++ Code/Northwind/Northwind/Program.cs
@@ -3,42 +3,57 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Orders;
+using Raven.Abstractions.Indexing;
 using Raven.Client.Document;
+using Raven.Client.Indexes;
 using Raven.Client.Linq;
 
 namespace Northwind
 {
-	public class Program
-	{
+    public class Program
+    {
 
-		private static void Main(string[] args)
-		{
-			//var documentStore = new DocumentStore
-			//{
-			//	Url = "http://localhost:8080",
-			//	DefaultDatabase = "repl3",
-			//};
+        private static void Main(string[] args)
+        {
+            //var documentStore = new DocumentStore
+            //{
+            //	Url = "http://localhost:8080",
+            //	DefaultDatabase = "repl3",
+            //};
 
-			//documentStore.Initialize();
+            //documentStore.Initialize();
 
 
-			//while (true)
-			//{
-			//	using (var session = documentStore.OpenSession())
-			//	{
-			//		var configs = session.Load<dynamic>(new[] { "config/add", "config/default" });
-			//	}
-			//}
+            //while (true)
+            //{
+            //	using (var session = documentStore.OpenSession())
+            //	{
+            //		var configs = session.Load<dynamic>(new[] { "config/add", "config/default" });
+            //	}
+            //}
 
-			var path =
-				@"D:\ravendb\Raven.Tests.Issues\bin\Release\RavenDB-1369.Backup\Inc 2014-12-22 13-54-01\IndexDefinitions\f02c0134-7ac1-4e9f-b20b-40122bde5948";
-			
-			var dir = Path.GetDirectoryName(path);
+            var path =
+                @"D:\ravendb\Raven.Tests.Issues\bin\Release\RavenDB-1369.Backup\Inc 2014-12-22 13-54-01\IndexDefinitions\f02c0134-7ac1-4e9f-b20b-40122bde5948";
 
-			//if (Directory.Exists(dir) == false)
-				Directory.CreateDirectory(dir);
-			
-			File.WriteAllText(path,path);
-		}
-	}
+            var dir = Path.GetDirectoryName(path);
+
+            //if (Directory.Exists(dir) == false)
+            Directory.CreateDirectory(dir);
+
+            File.WriteAllText(path, path);
+        }
+    }
+
+
+    public class Product_Search : AbstractIndexCreationTask<Product>
+    {
+        public Product_Search()
+        {
+            Map = products =>
+                from product in products
+                select new { product.Name };
+
+            Index(x => x.Name, FieldIndexing.Analyzed);
+        }
+    }
 }
--- current.ps1
+++ current.ps1
@@ -1,7 +1,7 @@
 Remove-Item  Output -Force -Recurse -ErrorAction SilentlyContinue | out-null 
 MkDir Output  | out-null
 
-$output = "Output\Current.pdf"
+$output = "Output\Current2.pdf"
 
 pandoc --epub-metadata=metadata.xml --standalone --highlight-style=tango --self-contained `
 	--number-sections --listings --latex-engine=xelatex -o $output .\Ch09\Ch09.md
