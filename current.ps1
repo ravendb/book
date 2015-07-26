5a7ac85e4fa2e11b4e2d86c39101b3927359d926
Ayende Rahien <ayende@ayende.com>
2015-07-26 14:45:52 +0300

    Starting to work on Chapter 11

--- current.ps1
+++ current.ps1
@@ -4,6 +4,6 @@ MkDir Output  | out-null
 $output = "Output\Current.pdf"
 
 pandoc --epub-metadata=metadata.xml --standalone --highlight-style=tango --self-contained `
-	--number-sections --listings --latex-engine=xelatex -o $output .\Ch10\Ch10.md
+	--number-sections --listings --latex-engine=xelatex -o $output .\Ch11\Ch11.md
 
 start $output
