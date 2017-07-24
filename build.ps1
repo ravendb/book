94f426dd438652ae29d35d4e06eca3b444f17d28
Ayende Rahien <ayende@ayende.com>
2017-07-24 09:29:39 +0300

    Moving a lot of the mode advanced topics of distributed ravendb to chapter 7

--- build.ps1
+++ build.ps1
@@ -13,7 +13,7 @@ pandoc --table-of-contents --toc-depth=3 --epub-metadata=metadata.xml `
 	--listings --latex-engine=xelatex --number-sections `
 	--epub-cover-image=.\Cover.jpg `
 	-o $output .\title.txt .\Intro\Intro.md .\Ch02\Ch02.md .\Ch03\Ch03.md .\Ch04\Ch04.md `
-	.\Ch05\Ch05.md .\Ch06\PartII.md  .\Ch06\Ch06.md 
+	.\Ch05\Ch05.md .\Ch06\PartII.md  .\Ch06\Ch06.md .\Ch07\Ch07.md 
 
 if($start) {
 	start $output
