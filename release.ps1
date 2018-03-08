07acedda04f845f5579134cd46b458118089528c
Ayende Rahien <Ayende@ayende.com>
2018-03-08 16:30:01 +0200

    TLS1.2

--- release.ps1
+++ release.ps1
@@ -4,6 +4,7 @@
 $token = Get-Content "$pwd\..\Credentials\github.txt"
 $release = "v4.0.11-preview"
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 $json = Invoke-WebRequest -ErrorAction Stop `
      -Uri "https://api.github.com/repos/ravendb/book/releases" `
