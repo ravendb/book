b9d9ff8bdc155a5fcdd53789fdc891096823a2c1
Ayende Rahien <Ayende@ayende.com>
2018-03-08 16:24:15 +0200

    Closing chapter 11

--- release.ps1
+++ release.ps1
@@ -2,7 +2,7 @@
 .\build.ps1 docx $false
 
 $token = Get-Content "$pwd\..\Credentials\github.txt"
-$release = "v4.0.10-preview"
+$release = "v4.0.11-preview"
 
 
 $json = Invoke-WebRequest -ErrorAction Stop `
