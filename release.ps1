a55639f372c6beea34efa147b4b7306ebda42923
Ayende Rahien <Ayende@ayende.com>
2018-03-29 19:27:45 +0300

    releasing chpater 15

--- release.ps1
+++ release.ps1
@@ -2,7 +2,7 @@
 .\build.ps1 docx $false
 
 $token = Get-Content "$pwd\..\Credentials\github.txt"
-$release = "v4.0.14-preview"
+$release = "v4.0.15-preview"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
