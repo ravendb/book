4f22697cb7564cec505a9606e31c56755fcb27c5
Ayende Rahien <Ayende@ayende.com>
2018-04-12 14:33:35 +0300

    Adding additional chapters

--- build.ps1
+++ build.ps1
@@ -16,7 +16,7 @@ pandoc --table-of-contents --toc-depth=3 --epub-metadata=metadata.xml `
 	.\Ch05\Ch05.md .\Ch06\PartII.md  .\Ch06\Ch06.md .\Ch07\Ch07.md .\Ch08\Ch08.md `
 	.\Ch09\PartIII.md  .\Ch09\Ch09.md .\Ch10\Ch10.md .\Ch11\Ch11.md .\Ch12\Ch12.md `
 	.\Ch13\PartIV.md .\Ch13\Ch13.md .\Ch14\Ch14.md .\Ch15\PartV.md .\Ch15\Ch15.md `
-	.\Ch16\Ch16.md 
+	.\Ch16\Ch16.md .\Ch17\Ch17.md .\Ch18\Ch18.md `
 
 if($start) {
 	start $output
