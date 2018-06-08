20042fdf7314007229f15b7600bfa9638342bb01
Ayende Rahien <Ayende@ayende.com>
2018-06-08 11:10:01 +0300

    Preparing for release

--- release.ps1
+++ release.ps1
@@ -2,7 +2,7 @@
 .\build.ps1 docx $false
 
 $token = Get-Content "$pwd\..\Credentials\github.txt"
-$release = "v4.0.20-rc"
+$release = "v4.0.21-rc"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
