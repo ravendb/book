741fe70d976f24f124dde3eb823e19c698fa46d7
Ayende Rahien <Ayende@ayende.com>
2018-06-17 10:59:17 +0300

    Going back to espresso for code style

--- build.ps1
+++ build.ps1
@@ -9,7 +9,7 @@ MkDir Output -ErrorAction SilentlyContinue  | out-null
 $output = ".\Output\Inside RavenDB 4.0.$type"
 
 pandoc --table-of-contents --toc-depth=3 --epub-metadata=metadata.xml `
-	--standalone --highlight-style=monochrome --self-contained --top-level-division=part `
+	--standalone --highlight-style=espresso --self-contained --top-level-division=part `
 	--listings --pdf-engine=xelatex --number-sections --css=pandoc.css `
 	--epub-cover-image=.\Cover.jpg --epub-embed-font=Styling/RobotoMono-Regular.ttf `
 	-o $output .\title.txt .\Intro\Intro.md .\Ch02\Ch02.md .\Ch03\Ch03.md .\Ch04\Ch04.md `
