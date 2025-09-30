382bcd54859a7e4d283b73072bb93faec27cb1bc
Ayende Rahien <Ayende@ayende.com>
2025-10-01 02:18:46 +0300

    Moving all the old chapters to the size

--- pdf.ps1
+++ pdf.ps1
@@ -1,4 +1,4 @@
-rm -r ./build
-mkdir ./build 
-asciidoctor-pdf -r asciidoctor-diagram --destination-dir ./output  .\book.adoc
-./output/book.pdf
+rm -r ./build | Out-Null
+mkdir ./build | Out-Null
+asciidoctor-pdf -r asciidoctor-diagram --destination-dir ./output  .\current.adoc
+./output/current.pdf
