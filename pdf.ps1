4fd063795b1312207f339dc9d0f223b7cff5d7a3
Ayende Rahien <Ayende@ayende.com>
2025-10-02 13:15:50 +0300

    Finishing chapter 3

--- pdf.ps1
+++ pdf.ps1
@@ -1,4 +1,4 @@
 rm -r ./build | Out-Null
 mkdir ./build | Out-Null
-asciidoctor-pdf -r asciidoctor-diagram --destination-dir ./output  .\current.adoc
-./output/current.pdf
+asciidoctor-pdf -r asciidoctor-diagram --destination-dir ./output  .\book.adoc
+./output/book.pdf
